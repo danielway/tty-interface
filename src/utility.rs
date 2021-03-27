edcd86a9b01e49077456aed6df40d8caf8e6f46a
Daniel Way <contact@danieldway.com>
2021-03-27 11:24:57 -0500

    docs(utility): add to all functions

--- src/utility.rs
+++ src/utility.rs
@@ -5,6 +5,7 @@ use termion::cursor;
 use crate::segment::Segment;
 use crate::line::Line;
 
+/// Moves the cursor to `to` and updates `cursor` with the new position.
 pub(crate) fn move_cursor(cursor: &mut CursorPosition, to: &CursorPosition) {
     // Move cursor vertically
     if from.y < to.y {
@@ -24,16 +25,25 @@ pub(crate) fn move_cursor(cursor: &mut CursorPosition, to: &CursorPosition) {
     cursor.x = to.x;
 }
 
+/// Clears the contents of the cursor's current line. Does not move the cursor.
 pub(crate) fn clear_line() {
     print!("{}", termion::clear::CurrentLine);
 }
 
+/// Clears the terminal line, renders the `line`, and updates the `cursor` position according to the
+/// `line`'s total length. Will move the `cursor` to `x=0` if `>0`.
 pub(crate) fn render_line(cursor: &mut CursorPosition, line: &Line) {
+    if cursor.x != 0 {
+        move_cursor(cursor, &CursorPosition::init(0, cursor.y));
+    }
+
+    clear_line();
     for segment in line.segments {
         render_segment(cursor, &segment);
     }
 }
 
+/// Renders the `segment` at `cursor` and advances `cursor` with `segment.text`'s length.
 pub(crate) fn render_segment(cursor: &mut CursorPosition, segment: &Segment) {
     // TODO: add color and style
     print!("{}", segment.text);
