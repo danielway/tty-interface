6384f189a30759c1bf256b0265808bc9ee236572
Daniel Way <contact@danieldway.com>
2021-03-27 10:02:42 -0500

    feat(utility): implement move_cursor

--- src/utility.rs
+++ src/utility.rs
@@ -1,7 +1,18 @@
 use crate::cursor::CursorPosition;
 use termion::raw::RawTerminal;
 use std::io::Stdout;
+use termion::cursor;
 
-pub(crate) fn move_cursor(stdout: &mut RawTerminal<Stdout>, from: &CursorPosition, to: &CursorPosition) {
-    // TODO: implement
+pub(crate) fn move_cursor(from: &CursorPosition, to: &CursorPosition) {
+    if from.y < to.y {
+        print!("{}", cursor::Down(to.y - from.y));
+    } else if from.y > to.y {
+        print!("{}", cursor::Up(from.y - to.y));
+    }
+
+    if from.x < to.x {
+        print!("{}", cursor::Right(to.x - from.x));
+    } else if from.x > to.x {
+        print!("{}", cursor::Left(from.x - to.x));
+    }
 }
