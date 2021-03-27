e9b1758b1b90af89467facd602437f72847e49e9
Daniel Way <contact@danieldway.com>
2021-03-27 09:41:02 -0500

    feat(segment): added logic for set segment

--- src/segment.rs
+++ src/segment.rs
@@ -1,6 +1,7 @@
 use crate::cursor::CursorPosition;
 use crate::update::UpdateStep;
 use crate::interface::InterfaceState;
+use crate::line::Line;
 
 pub struct Segment {
     pub text: String,
@@ -15,7 +16,19 @@ pub(crate) struct SetSegmentStep {
 }
 
 impl UpdateStep for SetSegmentStep {
-    fn do_update(&mut self, state: &mut InterfaceState, update_cursor: &mut CursorPosition) {}
+    fn do_update(&mut self, state: &mut InterfaceState, update_cursor: &mut CursorPosition) {
+        if self.line_index > state.lines.len() - 1 {
+            // TODO: throw error, line not added/set
+        }
+
+        if self.segment_index > state.lines[self.line_index].segments.len() {
+            // TODO: throw error, leaves gap in segments
+        }
+
+        state.lines[self.line_index].segments[self.segment_index] = self.segment.take().unwrap();
+        // TODO: rerender this segment
+        // TODO: if segment length differed, rerender segments after
+    }
 }
 
 pub(crate) struct DeleteSegmentStep {
