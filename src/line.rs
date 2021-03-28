b553959438cbd06ba4c1080d808d7607b463e397
Daniel Way <contact@danieldway.com>
2021-03-28 08:53:49 -0500

    feat(line): implement cursor movement for delete line

--- src/line.rs
+++ src/line.rs
@@ -2,6 +2,7 @@ use crate::segment::Segment;
 use crate::cursor::CursorPosition;
 use crate::update::UpdateStep;
 use crate::interface::InterfaceState;
+use crate::utility::{move_cursor_exact, render_line, move_cursor_by, clear_line};
 
 pub struct Line {
     pub segments: Vec<Segment>,
@@ -37,10 +38,18 @@ impl UpdateStep for DeleteLineStep {
             // TODO: throw error, line doesn't exist
         }
 
+        // If the cursor isn't on this line, move it here
+        let line_y: u16 = self.line_index as u16;
+        if update_cursor.y != line_y {
+            move_cursor_exact(update_cursor, 0, line_y);
+        }
+
+        // Shift lines >line_index down and render them; clear last (now shifted up) line
         state.lines.remove(self.line_index);
         for i in self.line_index..state.lines.len() {
-            // TODO: render shifted line
+            render_line(cursor, &state.lines[i]);
+            move_cursor_by(cursor, 0, 1);
         }
-        // TODO: clear previous last line
+        clear_line();
     }
 }
