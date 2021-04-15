c2d64d0ece8c55b421b8b9b69fe54f71973e270b
Daniel Way <contact@danieldway.com>
2021-04-15 15:30:57 -0500

    feat(examples/helloworld): create
    Add a simple two-segment example.

---
+++ examples/helloworld.rs
@@ -0,0 +1,24 @@
+extern crate tty_interface;
+
+use tty_interface::interface::TTYInterface;
+use termion::raw::IntoRawMode;
+use std::io::stdout;
+use tty_interface::line::Line;
+use tty_interface::segment::Segment;
+
+fn main() -> tty_interface::result::Result<()> {
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+
+    let mut tty = TTYInterface::new();
+
+    let mut batch = tty.start_update();
+    batch.set_line(0, Line {
+        segments: vec![
+            Segment { text: "Hello, world!".to_string() }
+        ]
+    });
+    tty.perform_update(&mut stdout, batch)?;
+
+    tty.end(&mut stdout)
+}
