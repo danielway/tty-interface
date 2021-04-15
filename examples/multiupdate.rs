0c3107ea7c36fd4ac4b2dd8447d88744a6485454
Daniel Way <contact@danieldway.com>
2021-04-15 15:31:39 -0500

    feat(examples/multiupdate): add a complex update sequence example

---
+++ examples/multiupdate.rs
@@ -0,0 +1,54 @@
+extern crate tty_interface;
+
+use tty_interface::interface::TTYInterface;
+use termion::raw::IntoRawMode;
+use std::io::stdout;
+use tty_interface::line::Line;
+use tty_interface::segment::Segment;
+
+fn main() -> tty_interface::result::Result<()> {
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+
+    let mut tty = TTYInterface::new();
+
+    for i in 1..=10 {
+        let mut batch = tty.start_update();
+
+        for j in 0..i {
+            batch.set_line(j, Line {
+                segments: vec![
+                    Segment { text: format!("Line {}", i - j) }
+                ]
+            });
+        }
+
+        tty.perform_update(&mut stdout, batch)?;
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+
+    for i in 0..10 {
+        let mut batch = tty.start_update();
+
+        batch.set_line(i, Line {
+            segments: vec![
+                Segment { text: format!("Line {}", i + 1) }
+            ]
+        });
+
+        tty.perform_update(&mut stdout, batch)?;
+
+        std::thread::sleep(std::time::Duration::from_millis(250));
+    }
+
+    for i in 0..10 {
+        let mut batch = tty.start_update();
+        batch.delete_line(9 - i);
+        tty.perform_update(&mut stdout, batch)?;
+
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+
+    tty.end(&mut stdout)
+}
