ee5e8c1abe6d0b8f950ab453beb5355378d236a6
Daniel Way <contact@danieldway.com>
2021-04-17 21:41:14 -0500

    refactor(examples/[helloworld/multistep]): remove termion usage
    Remove dependency on Termion to demonstrate that it's unnecessary.

--- examples/multistep.rs
+++ examples/multistep.rs
@@ -1,15 +1,11 @@
 extern crate tty_interface;
 
 use tty_interface::interface::TTYInterface;
-use termion::raw::IntoRawMode;
-use std::io::stdout;
 use tty_interface::line::Line;
 use tty_interface::segment::Segment;
 
 fn main() -> tty_interface::result::Result<()> {
-    let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap();
-
+    let mut stdout = std::io::stdout();
     let mut tty = TTYInterface::new(&mut stdout);
 
     let mut batch = tty.start_update();
