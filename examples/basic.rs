f10f500b9814b2d4d969215509bd306aa675b317
Daniel Way <contact@danieldway.com>
2022-10-15 22:28:11 -0500

    feat(examples/basic): add styling

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,6 @@
 use std::{thread::sleep, time::Duration};
 
-use tty_interface::{pos, Interface, Position, Result};
+use tty_interface::{pos, Interface, Position, Result, Style, Color};
 
 fn main() {
     execute().expect("execute basic example");
@@ -10,11 +10,11 @@ fn execute() -> Result<()> {
     let mut interface = Interface::for_stdout()?;
 
     interface.set(pos!(0, 0), "Hello, world!");
-    interface.set(pos!(5, 2), "Let's count to 10:");
+    interface.set_styled(pos!(5, 2), "Let's count to 10:", Style::default().set_italic(true));
     interface.apply()?;
 
     for i in 1..=10 {
-        interface.set(pos!(10, 3), &i.to_string());
+        interface.set_styled(pos!(10, 3), &i.to_string(), Style::default().set_bold(true).set_foreground(Color::Red));
         interface.apply()?;
         sleep(Duration::from_millis(250));
     }
